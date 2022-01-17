02da9519110e038c8796de4b24bee80219a90b91
Alex Nunes Feitosa <anfmidway@gmail.com>
2022-01-17 19:52:19 -0300

    Inicio de controller AuthController

---
+++ src/services/NSF.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSF.Identidade.API.Controllers
+{
+    public class AuthController
+    {
+    }
+}
