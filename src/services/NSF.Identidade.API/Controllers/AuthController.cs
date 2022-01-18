b772938ac68acbfcb2dcee33033bb0a35f323f16
Alex Nunes Feitosa <anfmidway@gmail.com>
2022-01-18 20:11:56 -0300

    Registrar

--- src/services/NSF.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSF.Identidade.API/Controllers/AuthController.cs
@@ -1,11 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSF.Identidade.API.Models;
 using System.Threading.Tasks;
+using System.Web.Http.ModelBinding;
 
 namespace NSF.Identidade.API.Controllers
 {
-    public class AuthController
+    [Route("api/identidade")]
+    public class AuthController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            {
+                var user = new IdentityUser
+                {
+                    UserName = usuarioRegistro.Email,
+                    Email = usuarioRegistro.Email,
+                    EmailConfirmed = true
+                };
+                var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return Ok();
+                }
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    userName: usuarioLogin.Email, password: usuarioLogin.Senha,
+                    isPersistent: false, lockoutOnFailure: true
+                    );
+                    
+                if (result.Succeeded)
+                {
+                    return Ok();
+                }
+                return BadRequest();
+            }
+        }
     }
 }
