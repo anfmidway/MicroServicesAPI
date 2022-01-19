5a0583bfdebce4a688ee7edec3d8c7a149ab24ed
ANFSUSAX <anfmidway@gmail.com>
2022-01-19 14:56:54 -0300

    Autenticar

--- src/services/NSF.Identidade.API/Migrations/20220119013859_initial.Designer.cs
+++ src/services/NSF.Identidade.API/Migrations/20220119013859_initial.Designer.cs
@@ -10,8 +10,8 @@ using NSF.Identidade.API.Data;
 namespace NSF.Identidade.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220117220440_Initial")]
-    partial class Initial
+    [Migration("20220119013859_initial")]
+    partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
--- src/services/NSF.Identidade.API/Startup.cs
+++ src/services/NSF.Identidade.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using NSF.Identidade.API.Data;
 using System;
 using System.Collections.Generic;
@@ -36,12 +37,26 @@ namespace NSF.Identidade.API
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            c.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise",
+                    Contact = new OpenApiContact() { Name = "Eduardo Pires", Email = "contato@desenvolvedor.io" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri(uriString: "https://opensource.org/licenses/MIT") }
+                }
+                )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
